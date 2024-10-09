0236d99d9f8e2cfcb88661aa88c27314cd02a300
Daniel Riverol <dannyriverol@hotmail.com>
2024-10-09 10:47:15 -0300

    actualizacion

--- clase_13/js/main.js
+++ clase_13/js/main.js
@@ -2,13 +2,12 @@ const contenedor = document.querySelector("#contenedor");
 const inputs = document.querySelectorAll("input");
 const p = document.querySelector("#message");
 const form = document.querySelector("#form-login");
-const listado= document.getElementById('listado')
+const listado = document.getElementById("lista");
 //variables auxiliares para acceder a los inputs por separado
 const inputUser = inputs[0];
 const inputPass = inputs[1];
 const check = inputs[2];
 const btnIngresar = inputs[3];
-
 
 const servicios = [
   { id: 1, nombre: "limpieza de cutis", precio: 1200, img: "limpieza.jpg" },
@@ -60,12 +59,13 @@ function filtrarServicio(arr, filtro) {
 
 // Función para crear estructura html
 function crearHtml(arr) {
+// validar si arr no es un array
   contenedor.innerHTML = "";
- 
+
   let html;
   for (const servicio of arr) {
     html = `<div class="card">
-                <img src=" ./img/${servicio.img}" alt="${servicio.nombre}">
+                <img src="./img/${servicio.img}" alt="${servicio.nombre}">
                 <hr>
                 <h3>${servicio.nombre}</h3>
                 <p>Precio: $${servicio.precio} </p>
@@ -78,5 +78,47 @@ function crearHtml(arr) {
   }
 }
 
-console.log(servicios);
-console.log(paises);
+paises.push("Nigeria")
+
+for (const pais of paises) {
+  const li = document.createElement("li");
+  li.textContent = pais;
+
+  listado.appendChild(li);
+}
+crearHtml(servicios)
+const buscar= inputs[4]
+
+buscar.addEventListener('keyup', ()=>{
+  console.log(buscar.value);
+  const filtrados= filtrarServicio(servicios, buscar.value)
+
+  console.log(filtrados);
+  crearHtml(filtrados)
+  
+})
+
+//almacenamiento local 5 y 10 MB
+
+//guardar informacion
+localStorage.setItem("saludo", "Hola tarola")
+localStorage.setItem('dark-mode', true)
+localStorage.setItem("edad", 41)
+//ver informacion
+const saludo= localStorage.getItem("saludo")
+const darkMode= localStorage.getItem("dark-mode")  === "true"
+const edad = Number(localStorage.getItem("edad"));
+// console.log(saludo);
+// console.log(darkMode);
+// console.log(edad);
+const usuario= {id: 1, nickname:"ElBarto"}
+const usuarioEnJSON= JSON.stringify(usuario)
+
+localStorage.setItem("user", usuarioEnJSON)
+
+const usuarioLS = JSON.parse(localStorage.getItem("user"));
+console.log(usuarioLS);
+console.log(usuarioLS.id);
+console.log(usuarioLS.nickname);
+//JSON (stringify) (parse)
+
